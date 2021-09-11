734c4e65b00bd9efc5a63027b3d4d723a6c8bbbb
DESKTOP-EPM5P8O\skyline <nipunchathuranga45@gmail.com>
2021-09-11 12:40:46 +0530

    new 1

---
+++ app/src/main/java/com/example/etubeattempt2/fragment1.java
@@ -0,0 +1,35 @@
+package com.example.etubeattempt2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class fragment1 extends Fragment {
+
+    TextView text1;
+    @Nullable
+    @Override
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v= inflater.inflate(R.layout.fragment1_layout,container,false);
+
+
+        text1=(TextView) v.findViewById(R.id.f1TextView1);
+
+
+
+
+
+
+
+
+        return v;
+    }
+}
